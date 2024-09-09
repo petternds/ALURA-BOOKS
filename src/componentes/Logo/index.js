64747f9db78a6f46f20490ce1192d343906ebe77
Peter <petternds@gmail.com>
2024-09-09 18:37:33 -0300

    containeirizando e usando o styled-components

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,16 +1,25 @@
-import './estilo.css'
 import logo from '../../imagens/logo.svg'
+import styled from 'styled-components';
+
+
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px;
+`
+
+const LogoImage = styled.img`
+    margin - right: 10 px;
+`
 
 function Logo() {
     return (
-        <div className='logo'>
-            <img
+        <LogoContainer>
+            <LogoImage
                 src={logo}
                 alt='logo alura books'
-                className='logo-img'>
-            </img>
+            />
             <p><strong>Alura</strong>Books</p>
-        </div>
+        </LogoContainer>
     );
 };
 
