0b9e82cec22152cd5cd5888edb89f24a432dcea8
Peter <petternds@gmail.com>
2024-09-10 06:19:44 -0300

    criando o GlobalStyle

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import Header from './componentes/Header';
 import styled from 'styled-components';
 
 const AppContainer = styled.div`
-      width: 100vw;
-      height: 100vh;
-      background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
-    
-    li {
-      list-style: none;
-    }
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
 `
 
 function App() {
