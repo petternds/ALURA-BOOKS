26e29229b0a7fba1c1f0fe75385f4ba6a1a31e80
Peter <petternds@gmail.com>
2024-09-11 17:08:29 -0300

    atualizando com novos livros

--- src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -3,7 +3,5 @@ import livro from '../../imagens/livro.png'
 export const livros = [
     { nome: 'Liderança em Design', id: 1, src: livro },
     { nome: 'Liderança em UX', id: 2, src: livro },
-    { nome: 'JavaScrip Assertivo', id: 3, src: livro },
-    { nome: 'Cangaçeiro em JavaScript', id: 4, src: livro },
-    { nome: 'Aventureiros do Java', id: 5, src: livro }
-];
+    { nome: 'Apache Kafka e Spring Boot', id: 3, src: livro },
+]
