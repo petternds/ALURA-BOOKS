c64889fc782e46f740050efd75f0321f3d2fe1df
Peter <petternds@gmail.com>
2024-09-24 06:23:46 -0300

    usando async e await

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,7 +1,7 @@
 import Input from "../Input";
 import styled from "styled-components";
-import { useState } from "react";
-import { livros } from './dadosPesquisa'
+import { useEffect, useState } from "react";
+import { getLivros } from "../../servicos/livros";
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165% );
@@ -46,7 +46,17 @@ const Resultado = styled.div`
 `
 
 function Pesquisa() {
-    const [livrosPesquisados, setLivrosPesquisados] = useState([]);    
+    const [livrosPesquisados, setLivrosPesquisados] = useState([]);
+    const [livros, setLivros] = useState([]);
+
+    useEffect(() => {
+        fetchLivros()
+    }, []);
+
+    async function fetchLivros() {
+        const livrosDaAPI = await getLivros();
+        setLivros(livrosDaAPI);
+    }
 
     return (
         <PesquisaContainer>
@@ -59,8 +69,8 @@ function Pesquisa() {
             }} />
             {livrosPesquisados.map(livro => (
                 <Resultado>
-                    <p>{livro.nome}</p>
                     <img src={livro.src} alt="#" />
+                    <p>{livro.nome}</p>
                 </Resultado>
             ))}
         </PesquisaContainer>
